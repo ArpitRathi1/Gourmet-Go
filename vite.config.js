f5328130f2bc8a8dda5509b319d8ff2f069050cd
ArpitRathi1 <rathi844@gmail.com>
2024-11-16 15:43:59 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow remote access (for AWS EC2 or similar environments)
+    port: 3000, // You can set your desired port
+    open: false, // Disable automatic browser opening
+  }
 })
